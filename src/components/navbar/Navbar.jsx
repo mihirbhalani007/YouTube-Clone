5635b7d6eb1ea7dd89b937bd5ed7c48e2021cecf
Mihir Patel <mihirbhalani18@gmail.com>
2024-09-18 11:49:35 +0530

    Add styling to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,22 +9,28 @@ import profile_icon from "../../assets/user_profile.jpg";
 
 const Navbar = () => {
   return (
-    <nav className="flex">
-      <div className="flex-none">
-        <img src={menu_icon} alt="menu-icon" />
-        <img src={logo} alt="menu-icon" />
+    <nav className="flex items-center justify-between p-2 bg-white shadow">
+      <div className="flex items-center space-x-2">
+        <img src={menu_icon} alt="Menu" className="h-6" />
+        <img src={logo} alt="Logo" className="h-8" />
       </div>
-      <div className="flex-1">
-        <div className="justify-center">
-          <input type="text" placeholder="Search.." />
-          <img src={search_icon} alt="search icon" />
-        </div>
+      <div className="flex items-center space-x-2">
+        <input
+          type="text"
+          placeholder="Search.."
+          className="border p-1 rounded"
+        />
+        <img src={search_icon} alt="Search" className="h-6 cursor-pointer" />
       </div>
-      <div className="flex-none">
-        <img src={upload_icon} alt="upload icon" />
-        <img src={more_icon} alt="more icon" />
-        <img src={notification_icon} alt="notification icon " />
-        <img src={profile_icon} alt="profile icon" />
+      <div className="flex items-center space-x-2">
+        <img src={upload_icon} alt="Upload" className="h-6 cursor-pointer" />
+        <img src={more_icon} alt="More" className="h-6 cursor-pointer" />
+        <img
+          src={notification_icon}
+          alt="Notifications"
+          className="h-6 cursor-pointer"
+        />
+        <img src={profile_icon} alt="Profile" className="h-8 rounded-full" />
       </div>
     </nav>
   );
